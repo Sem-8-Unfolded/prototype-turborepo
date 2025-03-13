261283a7cbad10a709e9630c1e0712325f3dbe56
Gökay <gokayatalay2@outlook.com>
2025-03-13 16:10:47 +0100

    Added playwright tests

--- apps/e-commerce/app/layout.tsx
+++ apps/e-commerce/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SessionProvider } from "@repo/shopify-auth/client";
 import Navbar from "../components/navbar";
-import "@/authConfig";
+// import "@/authConfig";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
+        <SessionProvider>
+          <Navbar />
+        </SessionProvider>
         {children}
       </body>
     </html>
